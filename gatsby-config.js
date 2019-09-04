43a0e6026c320382b86f6d00daaa237cde40d7ac
Sean <seand52@gmail.com>
2019-09-04 21:35:31 +0200

    change page title and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ const mqPacker = require('css-mqpacker');
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby TypeScript Boilerplate',
-    siteUrl: `https://gatsby-typescript-boilerplate.netlify.com`,
+    title: 'Sean Daryanani',
+    siteUrl: `https://www.seandm.com`,
   },
   plugins: [
     {
@@ -32,7 +32,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: './src/favicon.png', // This path is relative to the root of the site.
+        icon: './src/images/favicon-32x32.png', // This path is relative to the root of the site.
       },
     },
     {
