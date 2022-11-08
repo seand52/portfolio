ea83c9204f0daed44578458aa130412aa33ecffd
Sean <sean.daryananimelwani@thetrainline.com>
2022-11-08 22:19:26 +0100

    deploy best version ever

--- src/components/Header/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './index.module.scss';
-
-const Header: React.FunctionComponent = () => {
-  return <header className={styles.header} />;
-};
-
-export default Header;
